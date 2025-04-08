873075f5053fd9f441d66e22405cde76ab0fc0b9
HarshVardhanK35 <kasavardhan2001@gmail.com>
2025-04-08 22:49:39 +0530

    Basics of React

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import ReactAtom from "./assets/react-core-concepts.png"
+
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={ReactAtom} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
