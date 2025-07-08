75439c71c5d4ceff4c6f46d77a24e31f8b63615e
HarshVardhanK35 <kasavardhan2001@gmail.com>
2025-07-08 23:59:37 +0530

    The Wild_Oasis: Implementing Additional Features - Filter/Sort

--- React-Jonas/12-the-wild-oasis/Texts/20-Implementing-more-features.js
+++ React-Jonas/12-the-wild-oasis/Texts/20-Implementing-more-features.js
@@ -18,15 +18,82 @@
  * --------------------------------------------
  * (using buttons so users can filter cabins whether they require "discount/not")
  * - we can use "URL" to filter out options [if we use URL then we can access URL from anywhere inside a react-element-tree]
+ * 
+ * - three files modified here: ["Filter" | "CabinTableOperations" | CSS only file: "TableOperations"]
  * [code]
  * ------
+// >>> Filter:
+---
+// - STYLING
+const FilterButton = styled.button`
+  background-color: var(--color-grey-0);
+  border: none;
+${(props) =>
+props.active &&
+    css`
+        background-color: var(--color-brand-600);
+        color: var(--color-brand-50);
+    `
+}
+// - CODE ... 
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
 
+  // - for active prop
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          onClick={() => handleClick(option.value)}
+          key={option.label}
+          active={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+export default Filter;
+--------------------------------- Connected ---------------------------------- 
+// >>> CabinTableOperations
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
+    </TableOperations>
+  );
+}
+export default CabinTableOperations;
+ * 
+ * $ SUMMARY:
  * 
  * 
  * 
  * 
- * ! 1. Checking Out a Booking (+ Fixing a Small Bug)
- * --------------------------------------------------
+ * ! 3. Client-Side Sorting: Sorting Cabins
+ * ----------------------------------------
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  * ! 1. Checking Out a Booking (+ Fixing a Small Bug)
  * --------------------------------------------------
--- React-Jonas/12-the-wild-oasis/src/pages/Cabins.jsx
+++ React-Jonas/12-the-wild-oasis/src/pages/Cabins.jsx
@@ -6,12 +6,10 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
-  
   return (
-
     <>
       <Row type="horizontal">
-        <Heading as="h1"> cabins</Heading>
+        <Heading as="h1">Cabins</Heading>
         <CabinTableOperations />
       </Row>
       <Row type="vertical">
